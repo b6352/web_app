baa450812ac1577f0298735ba55ec4568fcde5ef
sooooota <b6352@oic.jp>
2017-12-13 13:39:44 +0900

    buy作成

---
+++ resources/views/buy.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('cart_view')
+    <li><a href="cart/index">カートの中身を見る</a></li>
+@endsection
+@section('content')
+<div class="container">
+    <h1>ご注文ありがとうございました</h1>
+</div>
+@endsection
